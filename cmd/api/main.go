684ec6823fff9c3c603960f227e975f249cd3fe2
Alex171228 <alexskywoker0@gmail.com>
2025-11-25 16:33:32 +0300

    Update main.go

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,43 +1,43 @@
 package main
 
 import (
-    "log"
-    "net/http"
+	"log"
+	"net/http"
+	"os"
 
-    "github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5"
 
-    "pz9-auth/internal/http/handlers"
-    "pz9-auth/internal/platform/config"
-    "pz9-auth/internal/repo"
+	"pz9-auth/internal/http/handlers"
+	"pz9-auth/internal/platform/config"
+	"pz9-auth/internal/repo"
 )
 
 func main() {
-    cfg := config.Load()
+	cfg := config.Load()
+	addr := os.Getenv("APP_ADDR")
+	db, err := repo.Open(cfg.DB_DSN)
+	if err != nil {
+		log.Fatal("db connect:", err)
+	}
 
-    db, err := repo.Open(cfg.DB_DSN)
-    if err != nil {
-        log.Fatal("db connect:", err)
-    }
+	if err := db.Exec("SET timezone TO 'UTC'").Error; err != nil {
+		log.Println("warning: failed to set timezone to UTC:", err)
+	}
 
-    if err := db.Exec("SET timezone TO 'UTC'").Error; err != nil {
-        log.Println("warning: failed to set timezone to UTC:", err)
-    }
+	users := repo.NewUserRepo(db)
+	if err := users.AutoMigrate(); err != nil {
+		log.Fatal("migrate:", err)
+	}
 
-    users := repo.NewUserRepo(db)
-    if err := users.AutoMigrate(); err != nil {
-        log.Fatal("migrate:", err)
-    }
+	auth := &handlers.AuthHandler{
+		Users:      users,
+		BcryptCost: cfg.BcryptCost,
+	}
 
-    auth := &handlers.AuthHandler{
-        Users:      users,
-        BcryptCost: cfg.BcryptCost,
-    }
-
-    r := chi.NewRouter()
-    r.Post("/auth/register", auth.Register)
-    r.Post("/auth/login", auth.Login)
-
-    const addr = ":8081"
-    log.Println("listening on", addr)
-    log.Fatal(http.ListenAndServe(addr, r))
+	r := chi.NewRouter()
+	r.Post("/auth/register", auth.Register)
+	r.Post("/auth/login", auth.Login)
+	
+	log.Println("listening on", addr)
+	log.Fatal(http.ListenAndServe(addr, r))
 }
